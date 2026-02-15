4cb9912e7585ba2a0cf4633e718be40828f375a0
Семен Егоров <spb.semyon@mail.ru>
2026-02-15 18:25:09 +0300

    step 3

---
+++ src/parser.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+
+const parsers = {
+  '.json': JSON.parse,
+};
+
+export default (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath)
+  const fileContent = fs.readFileSync(absolutePath, 'utf-8')
+  const extension = path.extname(filepath).toLowerCase()
+  const parse = parsers[extension]
+  return parse(fileContent)
+};
