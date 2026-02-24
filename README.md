50d92d37bfc58e022cb3533310ffde4149c16f26
Семен Егоров <spb.semyon@mail.ru>
2026-02-24 15:42:02 +0300

    add yaml

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Box781/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Box781/frontend-project-46/actions)
 
-https://asciinema.org/a/NanVoLdMu1Y3h1sT
+https://asciinema.org/a/NanVoLdMu1Y3h1sT - формат json
 
 [![Node CI](https://github.com/Box781/frontend-project-46/actions/workflows/ci.yml/badge.svg)](https://github.com/Box781/frontend-project-46/actions/workflows/ci.yml)
 
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Box781_frontend-project-46&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Box781_frontend-project-46)
+
+https://asciinema.org/a/eivFDP1riB7PDH3b - формат yml
--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -7,9 +7,30 @@ import diff from '../src/diff.js'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-test('Сравнение', () => {
+test('Сравнение json', () => {
   const filepath1 = path.join(__dirname, '__fixtures__', 'file1.json')
   const filepath2 = path.join(__dirname, '__fixtures__', 'file2.json')
+
+  const file1 = parseFile(filepath1)
+  const file2 = parseFile(filepath2)
+
+  const res = diff(file1, file2)
+
+  const expected = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`
+
+  expect(res).toBe(expected)
+})
+
+test('Сравнение yaml', () => {
+  const filepath1 = path.join(__dirname, '__fixtures__', 'file1.yml')
+  const filepath2 = path.join(__dirname, '__fixtures__', 'file2.yml')
 
   const file1 = parseFile(filepath1)
   const file2 = parseFile(filepath2)
